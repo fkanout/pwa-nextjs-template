e5132cf8fed62741b1c8666802d9e2bf46ca5599
Faisal KANOUT <faisal@kanout.com>
2021-04-24 16:48:34 +0200

    Update icon

--- pages/index.js
+++ pages/index.js
@@ -23,7 +23,7 @@ const Nav = () => (
       <link rel='manifest' href='/manifest.json' />
       <link href='/favicon-16x16.png' rel='icon' type='image/png' sizes='16x16' />
       <link href='/favicon-32x32.png' rel='icon' type='image/png' sizes='32x32' />
-      <link rel='apple-touch-icon' href='/apple-icon.png'></link>
+      <link rel='apple-touch-icon' href='/icon-192x192.png'></link>
       <meta name='theme-color' content='#317EFB' />
     </Head>
     <ul>
