9df3e9001e2a9165f9b33a6eeef8c4f0e303ddfe
Faisal KANOUT <faisal@kanout.com>
2021-04-25 15:36:59 +0200

    Fix ioredis

--- pages/api/v1/hello.js
+++ pages/api/v1/hello.js
@@ -1,14 +1,7 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 const Redis = require("ioredis");
-const client = new Redis({
-  port: 31790, // Redis port
-  host: "eu1-immortal-ferret-31790.upstash.io", // Redis host
-  password: "8447d921a22640c3a4be219b67620c05",
-});
-client.on("error", function (err) {
-  throw err;
-});
+let client = new Redis("rediss://:8447d921a22640c3a4be219b67620c05@eu1-immortal-ferret-31790.upstash.io:31790");
 
 export default (req, res) => {
   client.get("channels").then((channelsData) => {
