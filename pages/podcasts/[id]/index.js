fa812502793c9cab9e68bb56b11100e60f061d33
Faisal KANOUT <faisal@kanout.com>
2021-04-25 17:51:55 +0200

    Add new URL

--- pages/podcasts/[id]/index.js
+++ pages/podcasts/[id]/index.js
@@ -43,7 +43,7 @@ const Podcasts = (data) => {
         <div key={podcast.id} className="flex mb-5 align-middle rounded-xl pl-2 pr-2">
           <button className="flex-shrink-0 rounded-full h-12 w-12 mr-4 ml-4 self-center flex items-center justify-center text-green-500 focus:outline-none transition-colors duration-150 border border-green-500 focus:shadow-outline hover:bg-green-500 hover:text-white"
             onClick={async () => {
-              const urlHost = `https://days-of-allah.herokuapp.com/audio/${podcast.id}`
+              const urlHost = `https://doa.kanout.com/api/v1/podcasts/${podcast.id}`
               const { data: audio } = await axios(urlHost);
               setUrl(audio.url)
               setTitle(podcast.title)
