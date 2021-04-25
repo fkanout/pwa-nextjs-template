5de956f5f942c67989396c04ace89561fd97b0c6
Faisal KANOUT <faisal@kanout.com>
2021-04-25 15:28:24 +0200

    Update client

--- pages/api/v1/hello.js
+++ pages/api/v1/hello.js
@@ -1,17 +1,18 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-const redis = require("redis");
-const client = redis.createClient({
-  host: 'eu1-immortal-ferret-31790.upstash.io',
-  port: '31790',
-  password: '8447d921a22640c3a4be219b67620c05',
-  tls: {}
-});
-client.on("error", function (err) {
-  throw err;
-});
+
 
 
 export default (req, res) => {
+  const redis = require("redis");
+  const client = redis.createClient({
+    host: 'eu1-immortal-ferret-31790.upstash.io',
+    port: '31790',
+    password: '8447d921a22640c3a4be219b67620c05',
+    tls: {}
+  });
+  client.on("error", function (err) {
+    throw err;
+  });
   client.get("channels").then((channelsData) => {
     res.status(200).json(JSON.parse(channelsData))
   })
