c03d5e4b63e5aa3eab3a8831ccf52feb256c6739
Faisal KANOUT <faisal@kanout.com>
2021-04-25 15:14:42 +0200

    WIP

--- pages/api/v1/hello.js
+++ pages/api/v1/hello.js
@@ -1,5 +1,19 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+const redis = require("redis");
+const client = redis.createClient({
+  host: 'eu1-immortal-ferret-31790.upstash.io',
+  port: '31790',
+  password: '8447d921a22640c3a4be219b67620c05',
+  tls: {}
+});
+client.on("error", function (err) {
+  throw err;
+});
+
 
 export default (req, res) => {
-  res.status(200).json({ name: 'John Doe' })
+  client.get("channels").then((channelsData) => {
+    res.status(200).json(channelsData)
+  })
+
 }
--- pages/index.js
+++ pages/index.js
@@ -2,69 +2,57 @@ import React from 'react'
 import Link from 'next/link'
 import Head from 'next/head'
 
-const links = [
-  { href: 'https://zeit.co/now', label: 'ZEIT' },
-  { href: 'https://github.com/zeit/next.js', label: 'GitHub' }
-].map(link => ({
-  ...link,
-  key: `nav-link-${link.href}-${link.label}`
-}))
-
-const Nav = () => (
-  <nav>
-    <Head>
-      <meta charset='utf-8' />
-      <meta http-equiv='X-UA-Compatible' content='IE=edge' />
-      <meta name='viewport' content='width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no' />
-      <meta name='description' content='Description' />
-      <meta name='keywords' content='Keywords' />
-      <title>Next.js PWA Example</title>
-
-      <link rel='manifest' href='/manifest.json' />
-      <link href='/favicon-16x16.png' rel='icon' type='image/png' sizes='16x16' />
-      <link href='/favicon-32x32.png' rel='icon' type='image/png' sizes='32x32' />
-      <link rel='apple-touch-icon' href='/icon-192x192.png'></link>
-      <meta name='theme-color' content='#317EFB' />
-    </Head>
-    <ul>
-      <li>
-        <Link href='/'>
-          <a>Home</a>
-        </Link>
-      </li>
-      {links.map(({ key, href, label }) => (
-        <li key={key}>
-          <a href={href}>{label}</a>
-        </li>
-      ))}
-    </ul>
-
-    <style jsx>{`
-      :global(body) {
-        margin: 0;
-        font-family: -apple-system, BlinkMacSystemFont, Avenir Next, Avenir, Helvetica, sans-serif;
-      }
-      nav {
-        text-align: center;
-      }
-      ul {
-        display: flex;
-        justify-content: space-between;
-      }
-      nav > ul {
-        padding: 4px 16px;
-      }
-      li {
-        display: flex;
-        padding: 6px 8px;
-      }
-      a {
-        color: #067df7;
-        text-decoration: none;
-        font-size: 13px;
-      }
-    `}</style>
-  </nav>
-)
-
-export default Nav
+export default function Main(data) {
+  return (
+    <div>
+      <Head>
+        <meta charset='utf-8' />
+        <meta http-equiv='X-UA-Compatible' content='IE=edge' />
+        <meta name='viewport' content='width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no' />
+        <meta name='description' content='Description' />
+        <meta name='keywords' content='Keywords' />
+        <title>Days of Allah</title>
+        <link rel='manifest' href='/manifest.json' />
+        <link href='/favicon-16x16.png' rel='icon' type='image/png' sizes='16x16' />
+        <link href='/favicon-32x32.png' rel='icon' type='image/png' sizes='32x32' />
+        <link rel='apple-touch-icon' href='/icon-192x192.png'></link>
+        <meta name='theme-color' content='#317EFB' />
+      </Head>
+      <div>
+        {
+          data.channels.map(channel => {
+            return (
+              <Link
+                key={channel.channelId}
+                href={{
+                  pathname: '/podcasts/[id]',
+                  query: { id: channel.channelId },
+                }}
+              >
+                <div >
+                  <div>
+                    {channel.channelName}
+                  </div>
+                  <img src={"data:image/jpg;base64, " + channel.avatar} />
+                </div>
+              </Link>
+            )
+          })
+        }
+      </div >
+    </div>
+  )
+}
+export async function getStaticProps(context) {
+  let channels = [];
+  try {
+    const res = await fetch(`https://doa.kanout.com/api/v1/hello`)
+    channels = await res.json()
+  }
+  catch (error) {
+    console.log('error', error)
+  }
+  return {
+    props: { channels }, // will be passed to the page component as props
+  }
+}
